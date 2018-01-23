e98dc3387caf434ef2c887dc5aa97fd806771863
senthilr04 <senthilkumar.rajamanickam2@cognizant.com>
2018-01-23 09:19:03 -0700

    Security Issue

--- integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
@@ -1,5 +1,6 @@
 package test.pivotal.pal.tracker;
 
+
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.PathNotFoundException;
 import io.pivotal.pal.tracker.testsupport.TestScenarioSupport;
@@ -8,28 +9,37 @@ import org.junit.Before;
 import org.junit.Test;
 import test.pivotal.pal.tracker.support.ApplicationServer;
 import test.pivotal.pal.tracker.support.HttpClient;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Fail.fail;
 import static test.pivotal.pal.tracker.support.MapBuilder.jsonMapBuilder;
+
 public class FlowTest {
+
     private final HttpClient httpClient = new HttpClient();
     private final String workingDir = System.getProperty("user.dir");
+
     private ApplicationServer registrationServer = new ApplicationServer(workingDir + "/../applications/registration-server/build/libs/registration-server.jar", "8883");
-    private ApplicationServer allocationsServer = new ApplicationServer(workingDir + "/../applications/allocations-server/build/libs/allocations-server.jar", "8895");
+    private ApplicationServer allocationsServer = new ApplicationServer(workingDir + "/../applications/allocations-server/build/libs/allocations-server.jar", "8881");
     private ApplicationServer backlogServer = new ApplicationServer(workingDir + "/../applications/backlog-server/build/libs/backlog-server.jar", "8882");
     private ApplicationServer timesheetsServer = new ApplicationServer(workingDir + "/../applications/timesheets-server/build/libs/timesheets-server.jar", "8884");
+
     private String registrationServerUrl(String path) {
         return "http://localhost:8883" + path;
     }
+
     private String allocationsServerUrl(String path) {
-        return "http://localhost:8895" + path;
+        return "http://localhost:8881" + path;
     }
+
     private String backlogServerUrl(String path) {
         return "http://localhost:8882" + path;
     }
+
     private String timesheetsServerUrl(String path) {
         return "http://localhost:8884" + path;
     }
+
     private long findResponseId(HttpClient.Response response) {
         try {
             return JsonPath.parse(response.body).read("$.id", Long.class);
@@ -42,15 +52,18 @@ public class FlowTest {
             }
         }
     }
+
+
     @Before
     public void setup() throws Exception {
         registrationServer.startWithDatabaseName("tracker_registration_test");
         allocationsServer.startWithDatabaseName("tracker_allocations_test");
         backlogServer.startWithDatabaseName("tracker_backlog_test");
         timesheetsServer.startWithDatabaseName("tracker_timesheets_test");
-        ApplicationServer.waitOnPorts("8895", "8882", "8883", "8884");
+        ApplicationServer.waitOnPorts("8881", "8882", "8883", "8884");
         TestScenarioSupport.clearAllDatabases();
     }
+
     @After
     public void tearDown() {
         registrationServer.stop();
@@ -58,22 +71,28 @@ public class FlowTest {
         backlogServer.stop();
         timesheetsServer.stop();
     }
+
     @Test
     public void testBasicFlow() throws Exception {
         HttpClient.Response response;
+
         response = httpClient.get(registrationServerUrl("/"));
         assertThat(response.body).isEqualTo("Noop!");
+
         response = httpClient.post(registrationServerUrl("/registration"), jsonMapBuilder()
                 .put("name", "aUser")
                 .build()
         );
         long createdUserId = findResponseId(response);
         assertThat(createdUserId).isGreaterThan(0);
+
         response = httpClient.get(registrationServerUrl("/users/" + createdUserId));
         assertThat(response.body).isNotNull().isNotEmpty();
+
         response = httpClient.get(registrationServerUrl("/accounts?ownerId=" + createdUserId));
         long createdAccountId = findResponseId(response);
         assertThat(createdAccountId).isGreaterThan(0);
+
         response = httpClient.post(registrationServerUrl("/projects"), jsonMapBuilder()
                 .put("accountId", createdAccountId)
                 .put("name", "aProject")
@@ -81,24 +100,33 @@ public class FlowTest {
         );
         long createdProjectId = findResponseId(response);
         assertThat(createdProjectId).isGreaterThan(0);
+
         response = httpClient.get(registrationServerUrl("/projects?accountId=" + createdAccountId));
         assertThat(response.body).isNotNull().isNotEmpty();
+
+
         response = httpClient.get(allocationsServerUrl("/"));
         assertThat(response.body).isEqualTo("Noop!");
+
         response = httpClient.post(
                 allocationsServerUrl("/allocations"), jsonMapBuilder()
                         .put("projectId", createdProjectId)
                         .put("userId", createdUserId)
-                        .put("firstDay", "2015-05-17")
-                        .put("lastDay", "2015-05-26")
+                        .put("firstDay", "2017-12-06")
+                        .put("lastDay", "2017-12-07")
                         .build()
         );
+
         long createdAllocationId = findResponseId(response);
         assertThat(createdAllocationId).isGreaterThan(0);
+
         response = httpClient.get(allocationsServerUrl("/allocations?projectId=" + createdProjectId));
         assertThat(response.body).isNotNull().isNotEmpty();
+
+
         response = httpClient.get(backlogServerUrl("/"));
         assertThat(response.body).isEqualTo("Noop!");
+
         response = httpClient.post(backlogServerUrl("/stories"), jsonMapBuilder()
                 .put("projectId", createdProjectId)
                 .put("name", "A story")
@@ -106,19 +134,24 @@ public class FlowTest {
         );
         long createdStoryId = findResponseId(response);
         assertThat(createdStoryId).isGreaterThan(0);
+
         response = httpClient.get(backlogServerUrl("/stories?projectId" + createdProjectId));
         assertThat(response.body).isNotNull().isNotEmpty();
+
+
         response = httpClient.get(timesheetsServerUrl("/"));
         assertThat(response.body).isEqualTo("Noop!");
+
         response = httpClient.post(timesheetsServerUrl("/time-entries"), jsonMapBuilder()
                 .put("projectId", createdProjectId)
                 .put("userId", createdUserId)
                 .put("date", "2015-12-17")
                 .put("hours", 8)
-            .build()
+                .build()
         );
         long createdTimeEntryId = findResponseId(response);
         assertThat(createdTimeEntryId).isGreaterThan(0);
+
         response = httpClient.get(timesheetsServerUrl("/time-entries?projectId" + createdProjectId));
         assertThat(response.body).isNotNull().isNotEmpty();
     }
