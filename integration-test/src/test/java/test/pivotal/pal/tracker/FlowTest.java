b1bcd5ea480b6ea4470a990c0c59742798b505d7
senthilr04 <senthilkumar.rajamanickam2@cognizant.com>
2018-01-22 23:52:10 -0700

    Securing a distributed system

--- integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
@@ -15,14 +15,14 @@ public class FlowTest {
     private final HttpClient httpClient = new HttpClient();
     private final String workingDir = System.getProperty("user.dir");
     private ApplicationServer registrationServer = new ApplicationServer(workingDir + "/../applications/registration-server/build/libs/registration-server.jar", "8883");
-    private ApplicationServer allocationsServer = new ApplicationServer(workingDir + "/../applications/allocations-server/build/libs/allocations-server.jar", "8815");
+    private ApplicationServer allocationsServer = new ApplicationServer(workingDir + "/../applications/allocations-server/build/libs/allocations-server.jar", "8895");
     private ApplicationServer backlogServer = new ApplicationServer(workingDir + "/../applications/backlog-server/build/libs/backlog-server.jar", "8882");
     private ApplicationServer timesheetsServer = new ApplicationServer(workingDir + "/../applications/timesheets-server/build/libs/timesheets-server.jar", "8884");
     private String registrationServerUrl(String path) {
         return "http://localhost:8883" + path;
     }
     private String allocationsServerUrl(String path) {
-        return "http://localhost:8815" + path;
+        return "http://localhost:8895" + path;
     }
     private String backlogServerUrl(String path) {
         return "http://localhost:8882" + path;
@@ -48,7 +48,7 @@ public class FlowTest {
         allocationsServer.startWithDatabaseName("tracker_allocations_test");
         backlogServer.startWithDatabaseName("tracker_backlog_test");
         timesheetsServer.startWithDatabaseName("tracker_timesheets_test");
-        ApplicationServer.waitOnPorts("8815", "8882", "8883", "8884");
+        ApplicationServer.waitOnPorts("8895", "8882", "8883", "8884");
         TestScenarioSupport.clearAllDatabases();
     }
     @After
--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -39,7 +39,6 @@ public class ApplicationServer {
             .put("SPRING_DATASOURCE_URL", dbUrl)
             .put("EUREKA_CLIENT_ENABLED", "false")
             .put("RIBBON_EUREKA_ENABLED", "false")
-            .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
             .put("APPLICATION_OAUTH_ENABLED", "false")
             .build()
         );
@@ -57,7 +56,7 @@ public class ApplicationServer {
 
     private static void waitUntilServerIsUp(String port) throws InterruptedException {
         HttpClient httpClient = new HttpClient();
-        int timeout = 120;
+        int timeout = 360;
         Instant start = Instant.now();
         boolean isUp = false;
 
